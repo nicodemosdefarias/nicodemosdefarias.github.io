26c8fe2e2591cd0ef0b68452b5a19347f63bd74d
nicodemosdefarias <102600428+nicodemosdefarias@users.noreply.github.com>
2022-05-04 18:04:33 -0300

    Mudanças
    Tema de fundo, troca de "Sist Hidráulicos" para "Proj de Máquinas", animação das seções.

---
+++ js/carregamento.js
@@ -0,0 +1,14 @@
+
+$(window).on('load',function(){
+  $(".loader").fadeOut(0);
+  $(".content").fadeIn(0);
+})
+
+//$(window).on('load',function(){ //tirar
+//setTimeout(
+//  function()
+//  {
+//    $(".loader").fadeOut(100);
+//    $(".content").fadeIn(100);
+//  }, 1500);
+//}) //tirar
