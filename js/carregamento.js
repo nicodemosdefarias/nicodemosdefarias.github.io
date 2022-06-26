07655903656cb3ff86bb8b371d8192226bf9c58c
nicodemosdefarias <102600428+nicodemosdefarias@users.noreply.github.com>
2022-06-26 17:48:51 -0300

    Novo formato
    Ainda com idiomas em JavaScript

--- js/carregamento.js
+++ js/carregamento.js
@@ -3,12 +3,3 @@ $(window).on('load',function(){
   $(".loader").fadeOut(0);
   $(".content").fadeIn(0);
 })
-
-//$(window).on('load',function(){ //tirar
-//setTimeout(
-//  function()
-//  {
-//    $(".loader").fadeOut(100);
-//    $(".content").fadeIn(100);
-//  }, 1500);
-//}) //tirar
